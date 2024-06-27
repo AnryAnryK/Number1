665c113d4939d80b7d2d6809710bc991574ed61e
Andrey Kalinchenko <andkay@yandex.ru>
2024-06-27 15:28:31 +0300

    2024.06.27 _ Sprint1 _ Написал FlightBookingServiceTest - (10)

--- Sprint1/src/main/java/Main.java
+++ Sprint1/src/main/java/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Customer specialAgent= new Customer("James","Bond");
-//FlightBookingService flightBookingService = new FlightBookingService();
-//flightBookingService.bookFlight(1, specialAgent);
-//        System.out.println("Общее количество: "+ flightBookingService.getCurrentBookingCount());
+FlightBookingService flightBookingService = new FlightBookingService();
+flightBookingService.book(1, specialAgent);
+       System.out.println("Общее количество: "+ flightBookingService.getBookingsCount());
 
 //        HotelBookingService hotelBookingService = new HotelBookingService();
 //        hotelBookingService.bookHotel(1, specialAgent);
 //        hotelBookingService.cancel(1);
 
-        BookingService bookingService = new HotelBookingService();
-        bookingService.book(1, specialAgent);
-        bookingService.cancel(1);
-        System.out.println(bookingService.getBookingsCount());
+//        BookingService bookingService = new HotelBookingService();
+//        bookingService.book(1, specialAgent);
+//        bookingService.cancel(1);
+//        System.out.println(bookingService.getBookingsCount());
     }
 }
